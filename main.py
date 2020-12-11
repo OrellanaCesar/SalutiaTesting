e69b6196ffa5c38242f1feda7c74223e4761fd7f
OrellanaCesar <orellanacesar.1992@gmail.com>
2020-12-11 16:13:43 -0300

    correcion main

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from Users.Logout import Logout
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    print("teistng")
     # l = Login()
     # l.loginAdmin(False)
     # l.reset()
@@ -13,8 +14,8 @@ if __name__ == '__main__':
     # l.loginProfesional(False)
     # l = Login_inc()
     # l.login()l = Logout()
-#     # l.logout_admin()
-#     # l.logout_Secretario()
-#     # l.logout_Profesional()
-    #
+    # l.logout_admin()
+    # l.logout_Secretario()
+    # l.logout_Profesional()
+
 
